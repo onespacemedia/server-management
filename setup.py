3d5dd5cdae450a626f09b83ca144fea5be628ad3
Daniel Samuels <daniel.samuels1@gmail.com>
2015-02-27 16:59:49 +0000

    Update setup.py, bump version number for release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='onespacemedia-server-management',
-    version='0.1.1',
+    version='0.1.2',
     packages=[
         'server_management',
         'server_management.management',
