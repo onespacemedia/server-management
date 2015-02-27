319b6828f14aafe13f5e17c5ccbe47c287233028
Daniel Samuels <daniel.samuels1@gmail.com>
2015-02-27 17:03:37 +0000

    Update setup.py URL.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='onespacemedia-server-management',
-    version='0.1.2',
+    version='0.1.3',
     packages=[
         'server_management',
         'server_management.management',
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
     description='A set of server management tools used by Onespacemedia.',
     long_description=README,
-    url='http://www.onespacemedia.com/',
+    url='https://github.com/onespacemedia/server-management/',
     author='James Foley',
     author_email='jamesfoley@onespacemedia.com',
     install_requires=['django', 'fabric', 'ansible', 'requests', 'fabvenv'],
