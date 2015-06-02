e49309f59fdb7df49521b10b28f28631cbc6a303
Daniel Samuels <daniel.samuels1@gmail.com>
2015-06-02 17:41:16 +0100

    Add Sentry notifications.

--- server_management/management/commands/update.py
+++ server_management/management/commands/update.py
@@ -5,10 +5,155 @@ from fabvenv import virtualenv
 
 from _core import load_config
 
+import datetime
+import json
+import requests
+import os
+import sys
+
 
 class Command(BaseCommand):
 
+    endpoint = "https://hooks.slack.com/services/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"
+    channel = '#commits'
+    bot_name = "Update Bot"
+    bot_emoji = ":neckbeard:"
+    current_commit = os.popen("git rev-parse --short HEAD").read().strip()
+    remote = os.popen("git config --get remote.origin.url").read().split(':')[1].split('.')[0]
+
+    def _bitbucket_commit_url(self, commit):
+        return "<https://bitbucket.org/{}/commits/{commit}|{commit}>".format(
+            self.remote,
+            commit=commit,
+        )
+
+    def _bitbucket_diff_url(self, commit1, commit2):
+        return "<https://bitbucket.org/{}/branches/compare/{}..{}#diff|diff>".format(
+            self.remote,
+            commit1,
+            commit2,
+        )
+
+    def _notify_start(self):
+        self.start_time = datetime.datetime.now()
+
+        requests.post(self.endpoint, data={
+            'payload': json.dumps({
+                'channel': self.channel,
+                'username': self.bot_name,
+                'icon_emoji': self.bot_emoji,
+                'attachments': [{
+                    'fallback': 'Update of {} has begun.'.format(django_settings.SITE_NAME),
+                    'color': '#22A7F0',
+                    'fields': [
+                        {
+                            'title': 'Project',
+                            'value': django_settings.SITE_NAME,
+                            'short': True,
+                        },
+                        {
+                            'title': 'Update status',
+                            'value': 'Started',
+                            'short': True,
+                        },
+                        {
+                            'title': 'Commit hash',
+                            'value': self._bitbucket_commit_url(self.current_commit),
+                            'short': True,
+                        },
+                        {
+                            'title': 'User',
+                            'value': os.popen("whoami").read().strip(),
+                            'short': True,
+                        }
+                    ]
+                }]
+            })
+        })
+
+    def _notify_success(self):
+        self.end_time = datetime.datetime.now()
+
+        requests.post(self.endpoint, data={
+            'payload': json.dumps({
+                'channel': self.channel,
+                'username': self.bot_name,
+                'icon_emoji': self.bot_emoji,
+                'attachments': [{
+                    'fallback': 'Update of {} has completed successfully.'.format(django_settings.SITE_NAME),
+                    'color': 'good',
+                    'fields': [
+                        {
+                            'title': 'Project',
+                            'value': django_settings.SITE_NAME,
+                            'short': True,
+                        },
+                        {
+                            'title': 'Update status',
+                            'value': 'Successful',
+                            'short': True,
+                        },
+                        {
+                            'title': 'Duration',
+                            'value': '{}.{} seconds'.format(
+                                (self.end_time - self.start_time).seconds,
+                                str((self.end_time - self.start_time).microseconds)[:2],
+                            ),
+                            'short': True,
+                        },
+                        {
+                            'title': 'Commit range',
+                            'value': '{} to {} ({})'.format(
+                                self._bitbucket_commit_url(self.server_commit),
+                                self._bitbucket_commit_url(self.current_commit),
+                                self._bitbucket_diff_url(self.current_commit, self.server_commit)
+                            ),
+                            'short': True,
+                        }
+                    ]
+                }]
+            })
+        })
+
+    def _notify_failed(self, message):
+        requests.post(self.endpoint, data={
+            'payload': json.dumps({
+                'channel': self.channel,
+                'username': self.bot_name,
+
+                'icon_emoji': self.bot_emoji,
+                'attachments': [{
+                    'fallback': 'Update of {} has failed'.format(
+                        django_settings.SITE_NAME,
+                    ),
+                    'color': 'danger',
+                    'fields': [
+                        {
+                            'title': 'Project',
+                            'value': django_settings.SITE_NAME,
+                            'short': True,
+                        },
+                        {
+                            'title': 'Update status',
+                            'value': 'Failed',
+                            'short': True,
+                        },
+                        {
+                            'title': 'Error message',
+                            'value': message,
+                        }
+                    ]
+                }]
+            })
+        })
+
+    def handle_exception(self, exctype, value, traceback):
+        self._notify_failed(str(value))
+        sys.__excepthook__(exctype, value, traceback)
+
     def handle(self, *args, **options):
+        self._notify_start()
+
         # Load server config from project
         config = load_config()
 
@@ -23,6 +168,7 @@ class Command(BaseCommand):
             with settings(warn_only=True):
                 if not run('whoami'):
                     print "Failed to connect to remote server"
+                    self._notify_failed("Failed to connect to remote server")
                     exit()
 
         # Set local project path
@@ -38,6 +184,8 @@ class Command(BaseCommand):
 
         with settings(warn_only=True):
             with cd('/var/www/{}'.format(project_folder)):
+                self.server_commit = run("git rev-parse --short HEAD")
+
                 # Check which venv we need to use.
                 result = run("bash -c '[ -d venv ]'")
 
@@ -92,3 +240,8 @@ class Command(BaseCommand):
                           config['opbeat']['app_id'],
                           config['opbeat']['secret_token'],
                       ))
+
+        self._notify_success()
+
+
+sys.excepthook = Command().handle_exception
