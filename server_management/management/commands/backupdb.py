25e60f2e766ac98cb404f1595986f16c1d97efca
Daniel Samuels <daniel.samuels1@gmail.com>
2015-07-16 14:40:28 +0100

    Update backupdb to work with AWS.

--- server_management/management/commands/backupdb.py
+++ server_management/management/commands/backupdb.py
@@ -1,5 +1,5 @@
-# from django.conf import settings as django_settings
 from django.core.management.base import BaseCommand
+from django.utils.timezone import now
 
 from _core import load_config
 
@@ -12,22 +12,9 @@ class Command(BaseCommand):
         # Load server config from project
         config, remote = load_config(env)
 
-        # Define current host from settings in server config
-        env.host_string = remote['server']['ip']
-        env.user = 'root'
-        env.disable_known_hosts = True
-        env.reject_unknown_hosts = False
-
-        # Make sure we can connect to the server
-        with hide('output', 'running', 'warnings'):
-            with settings(warn_only=True):
-                if not run('whoami'):
-                    print "Failed to connect to remote server"
-                    exit()
-
         with settings(warn_only=True):
             # Dump the database on the server.
-            sudo("su - {user} -c 'pg_dump {name} -cOx -U {user} -f /home/{name}/{name}.sql --clean'".format(
+            sudo("su - {user} -c 'pg_dump {name} -cOx -U {user} -f /home/{user}/{name}.sql --clean'".format(
                 name=remote['database']['name'],
                 user=remote['database']['user'],
             ))
@@ -36,16 +23,18 @@ class Command(BaseCommand):
             local('mkdir -p ~/Backups/')
 
             # Pull the SQL file down.
-            local('scp {}@{}:/home/{}/{}.sql ~/backups/{}.sql'.format(
-                'root',
+            local('scp {} {}@{}:/home/{}/{}.sql ~/Backups/{}-{}.sql'.format(
+                '' if not hasattr(env, 'key_filename') else ' -i {} '.format(env.key_filename),
+                env.user,
                 env.host_string,
                 remote['database']['user'],
                 remote['database']['name'],
                 config['local']['database']['name'],
+                now().isoformat(),
             ))
 
             # Delete the file on the server.
-            run('rm /home/{}/{}.sql'.format(
+            sudo('rm /home/{}/{}.sql'.format(
                 remote['database']['user'],
                 remote['database']['name'],
             ))
