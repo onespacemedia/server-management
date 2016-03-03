cdca70c2957187171c7e47bf6a4a4617ac97f087
James Foley <jamesfoley@onespacemedia.com>
2016-03-03 11:03:53 +0000

    Added option to override deployment user per server

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='onespacemedia-server-management',
-    version='0.4.5',
+    version='0.4.6',
     packages=[
         'server_management',
         'server_management.management',
