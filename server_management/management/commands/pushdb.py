5913820566e7a9e01a31a9e0a46d414689bc6578
Daniel Samuels <daniel.samuels1@gmail.com>
2016-05-12 13:20:16 +0100

    Update pushdb with some bugfixes.

--- server_management/management/commands/pushdb.py
+++ server_management/management/commands/pushdb.py
@@ -1,4 +1,4 @@
-from _core import load_config, ServerManagementBaseCommand
+from _core import load_config, ServerManagementBaseCommand, run_tasks
 
 from fabric.api import *
 
@@ -28,19 +28,59 @@ class Command(ServerManagementBaseCommand):
                 remote['database']['name'],
             ))
 
+            # Define db tasks
+            db_tasks = [
+                {
+                    'title': "Drop database",
+                    'ansible_arguments': {
+                        'module_name': 'postgresql_db',
+                        'module_args': "name='{}' state=absent".format(
+                                       remote['database']['name']
+                        ),
+                        'sudo_user': 'postgres'
+                    }
+                },
+                {
+                    'title': "Ensure database is created",
+                    'ansible_arguments': {
+                        'module_name': 'postgresql_db',
+                        'module_args': "name='{}' encoding='UTF-8' lc_collate='en_GB.UTF-8' lc_ctype='en_GB.UTF-8' "
+                                       "template='template0' state=present".format(
+                                           remote['database']['name']
+                                       ),
+                        'sudo_user': 'postgres'
+                    }
+                },
+                {
+                    'title': "Ensure user has access to the database",
+                    'ansible_arguments': {
+                        'module_name': 'postgresql_user',
+                        'module_args': "db='{}' name='{}' password='{}' priv=ALL state=present".format(
+                            remote['database']['name'],
+                            remote['database']['user'],
+                            remote['database']['password']
+                        ),
+                        'sudo_user': 'postgres'
+                    }
+                },
+                {
+                    'title': "Ensure user does not have unnecessary privileges",
+                    'ansible_arguments': {
+                        'module_name': 'postgresql_user',
+                        'module_args': 'name={} role_attr_flags=NOSUPERUSER,NOCREATEDB state=present'.format(
+                            remote['database']['name']
+                        ),
+                        'sudo_user': 'postgres'
+                    }
+                }
+            ]
+            run_tasks(env, db_tasks)
+
             # Import the database file
             # sudo("su - {name} -c 'psql -q {name} < /tmp/{name}.sql > /dev/null 2>&1'".format(
-            with settings(sudo_user='postgres'):
-                sudo('dropdb {name}'.format(
-                    name=remote['database']['name']
-                ))
 
-                sudo('createdb {name} -E UTF-8 --lc-collate=en_GB.UTF-8 --lc-ctype=en_GB.UTF-8 -T template0 -O {user}'.format(
-                    name=remote['database']['name'],
-                    user=remote['database']['user'],
-                ))
-
-                sudo('psql -q {name} < /tmp/{name}.sql'.format(
+            with settings(sudo_user=remote['database']['user']):
+                sudo('psql {name} < /tmp/{name}.sql'.format(
                     name=remote['database']['name']
                 ))
 
