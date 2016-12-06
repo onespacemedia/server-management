e60614cff7429f351b5111867983cc20c7b8031e
Daniel Samuels <daniel@onespacemedia.com>
2016-12-06 14:47:22 +0000

    Bump version number to 0.5.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='onespacemedia-server-management',
-    version='0.4.10',
+    version='0.5.0',
     packages=[
         'server_management',
         'server_management.management',
