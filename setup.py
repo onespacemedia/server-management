9991b8e8b19e44b4a9934843f0ab8efc6fc389e6
Daniel Samuels <daniel@onespacemedia.com>
2017-08-18 16:49:38 +0100

    Bump version number to 3.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='onespacemedia-server-management',
-    version='3.0.1',
+    version='3.0.2',
     packages=[
         'server_management',
         'server_management.management',
