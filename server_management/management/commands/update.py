e6037188085be2e5c95f8b7fc1cffad16fe655ed
Daniel Samuels <daniel@onespacemedia.com>
2017-10-26 16:20:51 +0100

    Remove the Slack stuff from update, we weren't using it

--- server_management/management/commands/update.py
+++ server_management/management/commands/update.py
@@ -12,172 +12,9 @@ from ._core import load_config, ServerManagementBaseCommand
 
 class Command(ServerManagementBaseCommand):
 
-    slack_enabled = False
-    slack_endpoints = []
-
-    current_commit = os.popen('git rev-parse --short HEAD').read().strip()
-    remote = os.popen('git config --get remote.origin.url').read().split(':')[1].split('.')[0]
-    remote = 'production'
-
-    def _bitbucket_commit_url(self, commit):
-        return '<https://bitbucket.org/{}/commits/{commit}|{commit}>'.format(
-            self.remote,
-            commit=commit,
-        )
-
-    def _bitbucket_diff_url(self, commit1, commit2):
-        return '<https://bitbucket.org/{}/branches/compare/{}..{}#diff|diff>'.format(
-            self.remote,
-            commit2,
-            commit1,
-        )
-
-    def _notify_start(self):
-        if not self.slack_enabled:
-            return
-
-        self.start_time = datetime.datetime.now()
-
-        for endpoint in self.slack_endpoints:
-            requests.post(endpoint['url'], data={
-                'payload': json.dumps({
-                    'channel': endpoint['channel'],
-                    'username': endpoint['name'],
-                    'icon_emoji': endpoint['emoji'],
-                    'attachments': [{
-                        'fallback': 'Update of {} to {} has begun.'.format(
-                            django_settings.SITE_NAME,
-                            self.remote
-                        ),
-                        'color': '#22A7F0',
-                        'fields': [
-                            {
-                                'title': 'Project',
-                                'value': '{} ({})'.format(
-                                    django_settings.SITE_NAME,
-                                    self.remote,
-                                ),
-                                'short': True,
-                            },
-                            {
-                                'title': 'Update status',
-                                'value': 'Started',
-                                'short': True,
-                            },
-                            {
-                                'title': 'Commit hash',
-                                'value': self._bitbucket_commit_url(self.current_commit),
-                                'short': True,
-                            },
-                            {
-                                'title': 'User',
-                                'value': os.popen("whoami").read().strip(),
-                                'short': True,
-                            }
-                        ]
-                    }]
-                })
-            })
-
-    def _notify_success(self):
-        if not self.slack_enabled:
-            return
-
-        self.end_time = datetime.datetime.now()
-
-        for endpoint in self.slack_endpoints:
-            requests.post(endpoint['url'], data={
-                'payload': json.dumps({
-                    'channel': endpoint['channel'],
-                    'username': endpoint['name'],
-                    'icon_emoji': endpoint['emoji'],
-                    'attachments': [{
-                        'fallback': 'Update of {} to {} has completed successfully.'.format(
-                            django_settings.SITE_NAME,
-                            self.remote
-                        ),
-                        'color': 'good',
-                        'fields': [
-                            {
-                                'title': 'Project',
-                                'value': '{} ({})'.format(
-                                    django_settings.SITE_NAME,
-                                    self.remote,
-                                ),
-                                'short': True,
-                            },
-                            {
-                                'title': 'Update status',
-                                'value': 'Successful',
-                                'short': True,
-                            },
-                            {
-                                'title': 'Duration',
-                                'value': '{}.{} seconds'.format(
-                                    (self.end_time - self.start_time).seconds,
-                                    str((self.end_time - self.start_time).microseconds)[:2],
-                                ),
-                                'short': True,
-                            },
-                            {
-                                'title': 'Commit range',
-                                'value': '{} to {} ({})'.format(
-                                    self._bitbucket_commit_url(self.server_commit),
-                                    self._bitbucket_commit_url(self.current_commit),
-                                    self._bitbucket_diff_url(self.current_commit, self.server_commit)
-                                ),
-                                'short': True,
-                            }
-                        ]
-                    }]
-                })
-            })
-
-    def _notify_failed(self, message):
-        if not self.slack_enabled:
-            return
-
-        for endpoint in self.slack_endpoints:
-            requests.post(endpoint['url'], data={
-                'payload': json.dumps({
-                    'channel': endpoint['channel'],
-                    'username': endpoint['name'],
-                    'icon_emoji': endpoint['emoji'],
-                    'attachments': [{
-                        'fallback': 'Update of {} to {} has failed'.format(
-                            django_settings.SITE_NAME,
-                            self.remote
-                        ),
-                        'color': 'danger',
-                        'fields': [
-                            {
-                                'title': 'Project',
-                                'value': '{} ({})'.format(
-                                    django_settings.SITE_NAME,
-                                    self.remote,
-                                ),
-                                'short': True,
-                            },
-                            {
-                                'title': 'Update status',
-                                'value': 'Failed',
-                                'short': True,
-                            },
-                            {
-                                'title': 'Error message',
-                                'value': message,
-                            }
-                        ]
-                    }]
-                })
-            })
-
-    def handle_exception(self, exctype, value, traceback):
-        self._notify_failed(str(value))
-        sys.__excepthook__(exctype, value, traceback)
-
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
+
         parser.add_argument(
             '--force-update',
             action='store_true',
@@ -192,13 +29,6 @@ class Command(ServerManagementBaseCommand):
 
         # Set remote server name
         self.remote = config.get('remote_name')
-
-        # Load slack config
-        self.slack_enabled = config.get('slack', {'enabled': False})['enabled']
-        if self.slack_enabled:
-            self.slack_endpoints = config.get('slack', {'endpoints': []})['endpoints']
-
-        self._notify_start()
 
         # Set local project path
         local_project_path = django_settings.SITE_ROOT
@@ -300,8 +130,3 @@ class Command(ServerManagementBaseCommand):
                           config['opbeat']['app_id'],
                           config['opbeat']['secret_token'],
                       ))
-
-        self._notify_success()
-
-
-sys.excepthook = Command().handle_exception
