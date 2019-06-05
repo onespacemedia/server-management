e15140eb76ffd6845ac1bee750620d1f01dce7ac
Lewis Collard <lewis.collard@onespacemedia.com>
2019-06-05 15:38:32 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-VERSION = '3.1.0'
+VERSION = '3.1.1'
+
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
